e487c743ebeb5a11cbb63c4ec7e2fe7b7241d8d9
victorpaularony <smallypauls@gmail.com>
2025-02-20 10:21:57 +0300

    UPDATE: added db for comments like

--- internal/database/database.go
+++ internal/database/database.go
@@ -19,30 +19,21 @@ func InitializeDB() error {
 	// create the database queries tables
 	query := `
 	CREATE TABLE IF NOT EXISTS users(
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-        email TEXT UNIQUE NOT NULL,
-        username TEXT UNIQUE NOT NULL,
-        password TEXT NOT NULL,
-        image_path TEXT
+		id TEXT PRIMARY KEY,
+		useremail TEXT UNIQUE NOT NULL,
+		username TEXT UNIQUE NOT NULL,
+		password TEXT NOT NULL,
 		created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 	);
-
-	CREATE TABLE IF NOT EXISTS sessions (
-        id TEXT PRIMARY KEY,
-        user_id INTEGER,
-        expires_at DATETIME NOT NULL,
-        created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-        FOREIGN KEY (user_id) REFERENCES users(id)
-    );
 
 	CREATE TABLE IF NOT EXISTS posts(
 		id TEXT PRIMARY KEY,
 		user_id TEXT NOT NULL,
 		category TEXT NOT NULL, 
 		title TEXT NOT NULL,
-		content TEXT NOT NULL, 
-		media BLOB, 
-		content_type TEXT,
+		content TEXT NOT NULL, --discription of the post
+		media BLOB, --binary data - video, image and GIFs
+		content_type TEXT, --content type tracking(text, image, gif)
 		created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
 		FOREIGN KEY (user_id) REFERENCES users(id),
 		FOREIGN KEY (category) REFERENCES post_categories(category)
@@ -65,7 +56,7 @@ func InitializeDB() error {
 		FOREIGN KEY (user_id) REFERENCES users(id)
 	);
 
-	CREATE TABLE IF NOT EXISTS likes(
+	CREATE TABLE IF NOT EXISTS post_likes(
 		id TEXT PRIMARY KEY,
 		user_id TEXT NOT NULL,
 		post_id TEXT,
@@ -73,8 +64,15 @@ func InitializeDB() error {
 		FOREIGN KEY (user_id) REFERENCES users(id),
 		FOREIGN KEY (post_id) REFERENCES posts(id)
 	);
+	CREATE TABLE IF NOT EXISTS comment_likes(
+		id TEXT PRIMARY KEY,
+		user_id TEXT NOT NULL,
+		comment_id TEXT NOT NULL,
+		type TEXT CHECK(type IN('like', 'dislike')),
+		FOREIGN KEY (user_id) REFERENCES users(id),
+		FOREIGN KEY (comment_id) REFERENCES comments(id)
 
-	`
+	);`
 
 	if _, err := dataBase.Exec(query); err != nil {
 		dataBase.Close()
